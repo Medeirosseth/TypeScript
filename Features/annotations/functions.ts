e37fa42e0cd904331eba1a8e61d2ffa35242d1ae
Seth <Medeirosseth@gamil.com>
2021-10-30 13:17:35 -0700

    object destruction annotations

--- Features/annotations/functions.ts
+++ Features/annotations/functions.ts
@@ -2,3 +2,49 @@ const add = (a: number, b: number): number => {
 return ( a + b )
 }
 
+const subtract = (a: number, b: number): number => {
+  return a - b 
+} 
+
+// add a return annotation to show that a function acutally has a return statement 
+
+
+function divide(a: number, b: number ): number {
+  return a / b
+}
+
+const multiply = function(a: number, b: number): number {
+  return a * b; 
+}
+
+
+// for a string function that returns nothing / null => void
+const logger = (message: string ): void => {
+  console.log(message)
+}
+
+
+///very rare use
+const throwError = (message: string): never  => {
+  throw new Error(message)
+}
+
+const throwAError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+}
+
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+const logWeather = ({date, weather}: {date: Date, weather: string}): void => {
+  console.log(date);
+  console.log(weather)
+}
+
+
+logWeather(todaysWeather)
