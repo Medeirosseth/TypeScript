40b6d560c65ef09c51539604df0fcb72cb3b2f3e
Seth <Medeirosseth@gamil.com>
2021-11-04 14:13:12 -0700

    final commit

--- Features/classes.ts
+++ Features/classes.ts
@@ -1,14 +1,35 @@
-class Vehicle {
-  drive(): void {
-    console.log('chugga chugga')
-  }
+///modifiers 
+// Public -> this Method can be called anywhere, anytime 
+// Private -> this method can only  be called by other methods in this class
+// Protected-> this method can be called by other methods in THIS class, or by other methods in child classes
 
-  honk(): void {
+class Vehicle {
+
+  constructor(public color: string) {}
+
+
+  protected honk(): void {
     console.log('beep beep')
   }
 }
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color)
 
+class Car extends Vehicle{
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+  constructor(public wheels: number, color: string) {
+    super(color)
+  }
+  private drive(): void {
+    console.log('VROOM!')
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, 'red');
+car.startDrivingProcess();
+
